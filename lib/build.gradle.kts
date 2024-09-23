ea4cc247ee94b869d6e137fcac2b2636ef919940
Lourenzo Ferreira <lourenzo.ferreira@comprealugueagora.com.br>
2024-09-23 11:37:54 -0300

    chore: update settings using correct DSL
    @see: https://github.com/gradle/gradle/issues/30342

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,29 +6,30 @@
  */
 
 plugins {
-    // Apply the java-library plugin for API and implementation separation.
-    `java-library`
+  // Apply the java-library plugin for API and implementation separation.
+  `java-library`
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
-    mavenCentral()
+  // Use Maven Central for resolving dependencies.
+  mavenCentral()
 }
 
 dependencies {
-    // Use JUnit test framework.
-    testImplementation(libs.junit)
+  implementation("org.projectlombok:lombok:1.18.22")
+  // Use JUnit test framework.
+  testImplementation(libs.junit)
 
-    // This dependency is exported to consumers, that is to say found on their compile classpath.
-    api(libs.commons.math3)
+  // This dependency is exported to consumers, that is to say found on their compile classpath.
+  api(libs.commons.math3)
 
-    // This dependency is used internally, and not exposed to consumers on their own compile classpath.
-    implementation(libs.guava)
+  // This dependency is used internally, and not exposed to consumers on their own compile classpath.
+  implementation(libs.guava)
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
-    }
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(17))
+  }
 }
